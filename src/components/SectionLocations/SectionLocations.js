7f14b5183533936b748f7f780aae6565690e9e36
Deborah <ojomodeborah@hotmail.co.uk>
2021-07-06 22:52:41 +0100

    lading pagee

--- src/components/SectionLocations/SectionLocations.js
+++ src/components/SectionLocations/SectionLocations.js
@@ -9,8 +9,6 @@ import { NamedLink } from '../../components';
 import css from './SectionLocations.module.css';
 
 import nyImage from './images/ny-yogi.jpg';
-import laImage from './images/la-yogi.jpg';
-import sfImage from './images/sf-yogi.jpg';
 
 class LocationImage extends Component {
   render() {
@@ -49,23 +47,47 @@ const SectionLocations = props => {
       <div className={css.title}>
         <FormattedMessage id="SectionLocations.title" />
       </div>
-      <div className={css.locations}>
-        {locationLink(
-          'New York',
-          nyImage,
-          '?address=New%20York%20City%2C%20New%20York%2C%20USA&bounds=40.917576401307%2C-73.7008392055224%2C40.477399%2C-74.2590879797556'
-        )}
-        {locationLink(
-          'Los Angeles',
-          laImage,
-          '?address=Los%20Angeles%2C%20California%2C%20USA&bounds=34.161440999758%2C-118.121305008073%2C33.9018913203336%2C-118.521456965901'
-        )}
-        {locationLink(
-          'San Francisco',
-          sfImage,
-          '?address=San%20Francisco%2C%20California%2C%20USA&bounds=37.8324430069081%2C-122.354995082683%2C37.6044780500533%2C-122.517910874663'
-        )}
-      </div>
+   
+      <ul className={css.steps}>
+
+        <li className={css.step}>
+          <h2 className={css.stepTitle}>
+            <FormattedMessage id="SectionLocations.part1Title" />
+          </h2>
+          <p>
+            <FormattedMessage id="SectionLocations.part1Text" />
+          </p>
+        </li>
+
+        <li className={css.step}>
+          <h2 className={css.stepTitle}>
+            <FormattedMessage id="SectionLocations.part2Title" />
+          </h2>
+          <p>
+            <FormattedMessage id="SectionLocations.part2Text" />
+          </p>
+        </li>
+
+        <li className={css.step}>
+          <h2 className={css.stepTitle}>
+            <FormattedMessage id="SectionLocations.part3Title" />
+          </h2>
+          <p>
+            <FormattedMessage id="SectionLocations.part3Text" />
+          </p>
+        </li>
+
+        <li className={css.step}>
+          <h2 className={css.stepTitle}>
+            <FormattedMessage id="SectionLocations.part4Title" />
+          </h2>
+          <p>
+            <FormattedMessage id="SectionLocations.part4Text" />
+          </p>
+        </li>
+
+
+      </ul>
     </div>
   );
 };
